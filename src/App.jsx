cca854b5a7c5c27cdc9f98b62029f9f81ae73218
ichami630 <brandonichami@gmail.com>
2025-06-06 15:42:21 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Navigate,BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
